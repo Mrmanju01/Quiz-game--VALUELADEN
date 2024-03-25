a18a465bc3156df1fdfe2013daa113b7539c5526
Mrmanju01 <134767789+Mrmanju01@users.noreply.github.com>
2024-03-25 14:23:20 +0530

    Update settings.js

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,12 +1,11 @@
-// ****************************************Game Settings****************************************
+// *****************************************Game Settings*****************************************
 
-// selecting and getting difficulty level and categories
-const difficulty = document.querySelector("#difficulty");
+// selecting and getting diffculty level and categories
+const difficulty = document.querySelector("#difficulty");// also without Arrayfrom and then p2
 const categories = document.querySelector("#categories");
 const gameProgress = document.querySelector('#wrapper');
 let difficultyLevel;
 let categoryId;
-
 // toggle sound icon on
 const iconOn = document.querySelector("#off");
 
@@ -15,13 +14,12 @@ switcher = () => {
 };
 
 iconOn.addEventListener("click", switcher);
-
 // Click Sound when volume on
 const click = document.querySelector("#btnClick");
 const allButtons = document.querySelectorAll(".btn");
 
 clickBtn = () => {
-    if (document.querySelector(".fa-volume-up")) {
+    if (document.querySelector(".fa-volume-up ")) {
         click.play();
     }
 };
@@ -29,10 +27,13 @@ Array.from(allButtons).forEach((a) => {
     a.addEventListener("click", clickBtn);
 });
 
-difficulty.addEventListener("change", (e) => {
-    difficultyLevel = e.target.value;
-});
 
-categories.addEventListener("change", (e) => {
-    categoryId = e.target.value;
-});
+    difficulty.addEventListener("change", (e) => {
+        difficultyLevel = e.target.value;
+    });
+
+
+
+    categories.addEventListener("change", (e) => {
+        categoryId = e.target.value;
+    });
