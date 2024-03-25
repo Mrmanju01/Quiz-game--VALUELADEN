367b2e8c9c668b8f61daf283ee195bbbb25bf4c9
Mrmanju01 <134767789+Mrmanju01@users.noreply.github.com>
2024-03-25 14:24:15 +0530

    Update game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,22 +3,35 @@ const question = document.querySelector("#question");
 const currentScore = document.querySelector("#currentScore");
 const progressBar = document.querySelector(".qz-progress-bar");
 const gameDashboard = document.querySelector("#gameDashboard");
-const answers = document.querySelector("#answers");
 
-let displayedQuestion = null;
-let acceptAnswer = false;
+let displayedQuestion = [];
+let acceptAnswer = false; //set to false so user can't answer before new question loaded
 let score = 0;
 let counterQuestion = 0;
-let availableQuestions = [];
-let bonus;
+
+let questions = [];
 
 const EASY = "easy", MEDIUM = "medium", HARD = "hard";
 const MAX_QUESTIONS = 10;
 
+// Buttons
+const categoryBtn = document.querySelector("#categories");
+const difficultyBtn = document.querySelector("#difficulty");
+// Sounds
+const incorrectSound = document.querySelector("#incorrect");
+const correctSound = document.querySelector("#correct");
+const finalSound = document.querySelector("#finalSound");
+const fanfare = document.querySelector('#fanfare');
+const noPoints = document.querySelector('#noPoints');
+
+
+// Bonus point will depend on the level of quiz difficulty the player will chose
+let bonus;
 const startGame = () => {
     counterQuestion = 0;
     score = 0;
-    availableQuestions = [...questions];
+    availableQuestions = [...questions]; // creating full copy of questions
+    const answers = document.querySelector("#answers");
 
     if (difficultyLevel === EASY) {
         bonus = 10;
@@ -31,50 +44,55 @@ const startGame = () => {
     if (!difficultyLevel || !categoryId) {
         $('#settingsModal').modal('show');
     } else {
+        answers.innerHTML = "";
         location.href = "#start";
+        gameProgress.classList.remove("hide");
         gameDashboard.classList.add("display");
         getNextQuestion();
     }
+
 };
 
 const getNextQuestion = () => {
-    counterQuestion++;
-    if (counterQuestion <= MAX_QUESTIONS) {
-        progressBar.innerText = `${counterQuestion}/${MAX_QUESTIONS}`;
-    }
-
+    //temprorary setting quiz after questions finish to refresh page so game starts again
     if (availableQuestions.length === 0) {
+        question.innerHTML = "";
         $('#finalModal').modal('show');
         setHighScore();
         gameDashboard.classList.remove("display");
         return;
     }
+    counterQuestion++;
+    if (counterQuestion <= 10) {
+        progressBar.innerText = `${counterQuestion}/${MAX_QUESTIONS}`;
+    }
 
-    const indexQuestion = Math.floor(Math.random() * availableQuestions.length);
-    displayedQuestion = availableQuestions[indexQuestion];
-    question.innerText = `${counterQuestion}. ${decodeURIComponent(displayedQuestion.question)}`;
+    const indexQuestion = Math.floor(Math.random() * availableQuestions.length); //to get random number depending on number questions available
+    displayedQuestion = availableQuestions[indexQuestion]; // displaying random order question
+    question.innerText = `${counterQuestion}. ${decodeURIComponent(displayedQuestion.question)}`; //displaying question by calling question property
 
-    answers.innerHTML = "";
-    displayedQuestion.possible_answers.forEach((answer, index) => {
+    const possibleAnswers = displayedQuestion.possible_answers;
+    possibleAnswers.forEach((answer, index) => {
         const alphabet = ["A", "B", "C", "D"];
-        const choiceContainer = document.createElement('div');
-        choiceContainer.classList.add('answers-container', 'col-12', 'col-sm-5');
-        choiceContainer.innerHTML = `
-            <p class="answer-prefix">${alphabet[index]}</p>
-            <p class="answer-choice" data-number=${index + 1}>${decodeURIComponent(answer)}</p>`;
-        answers.appendChild(choiceContainer);
+        answer = answers.innerHTML += `
+        <div class="answers-container col-12 col-sm-5">
+                        <p class="answer-prefix ">${alphabet[index]}</p>
+                        <p class="answer-choice " data-number=${index + 1}></p>
+                    </div>`;
     });
 
     const choices = document.querySelectorAll(".answer-choice");
     choices.forEach(choice => {
         const number = choice.dataset.number;
-        if (choice.innerText === decodeURIComponent(displayedQuestion.correct_answer)) {
+        choice.innerText = decodeURIComponent(displayedQuestion["choice" + number]);
+        if (choice.innerText === decodeURIComponent(displayedQuestion.correct_answer))
             choice.setAttribute("id", "correct_answer");
-        }
     });
 
+    //sync counter Question with progress bar
     progressBar.style.width = `${(counterQuestion / MAX_QUESTIONS) * 100}%`;
-    availableQuestions.splice(indexQuestion, 1);
+
+    availableQuestions.splice(indexQuestion, 1); //to remove old question and make space for new question
     acceptAnswer = true;
     selectingChoice();
 };
@@ -93,14 +111,26 @@ const selectingChoice = () => {
             if (answerClass === "correct") {
                 score += bonus;
                 currentScore.innerText = score;
+                if (document.querySelector(".fa-volume-up ")) {
+                    if (!incorrectSound || !correctSound) return;
+                    correctSound.currentTime = 0;
+                    correctSound.play();
+                }
+            } else {
+                if (document.querySelector(".fa-volume-up ")) {
+                    if (!incorrectSound || !correctSound) return;
+                    incorrectSound.currentTime = 0;
+                    incorrectSound.play();
+                }
             }
-
+            correctAnswer.parentElement.classList.add('correct');
             selectedChoice.parentElement.classList.add(answerClass);
             setTimeout(() => {
+                correctAnswer.parentElement.classList.remove('correct');
                 selectedChoice.parentElement.classList.remove(answerClass);
+                answers.innerHTML = "";
                 getNextQuestion();
             }, 600);
         });
     });
 };
-
